bb05520b1554234fcae616acd10b7d477f27108f
Nickpodski <Nickpodski@gmail.com>
2021-08-25 13:49:20 -0400

    finished lesson 4

--- Employee.cs
+++ Employee.cs
@@ -1,25 +1,34 @@
-namespace CatWorx.BadgeMaker {
+namespace CatWorx.BadgeMaker
+{
   class Employee
   {
     private string FirstName;
     private string LastName;
     private int Id;
     private string PhotoUrl;
-    public Employee(string firstName, string lastName, int id, string photoUrl) {
-        FirstName = firstName;
-        LastName = lastName;
-        Id = id;
-        PhotoUrl = photoUrl;
+    public Employee(string firstName, string lastName, int id, string photoUrl)
+    {
+      FirstName = firstName;
+      LastName = lastName;
+      Id = id;
+      PhotoUrl = photoUrl;
     }
-    public string GetName() {
-        return  FirstName + " " + LastName;
+    public string GetName()
+    {
+      return FirstName + " " + LastName;
     }
-  
-    public int GetId() {
-        return Id;
+
+    public int GetId()
+    {
+      return Id;
     }
-    public string GetPhotoUrl() {
-        return PhotoUrl;
+    public string GetPhotoUrl()
+    {
+      return PhotoUrl;
+    }
+    public string GetCompanyName()
+    {
+      return "Cat Worx";
     }
   }
 }
