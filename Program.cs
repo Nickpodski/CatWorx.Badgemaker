abfa2848912edd8d900f6af2993a55444233b916
Nickpodski <Nickpodski@gmail.com>
2021-08-26 16:33:24 -0400

    cleaned up the code a bit

--- Program.cs
+++ Program.cs
@@ -5,22 +5,24 @@ namespace CatWorx.BadgeMaker
 {
   class Program
   {
+    static void Next(List<Employee> employees)
+    {
+      Util.PrintEmployees(employees);
+      Util.MakeCSV(employees);
+      Util.MakeBadges(employees);
+    }
     static void Main(string[] args)
     {
       bool response = PeopleFetcher.CheckTask();
       if (response == true)
       {
         List<Employee> employees = PeopleFetcher.GetFromAPI();
-        Util.PrintEmployees(employees);
-        Util.MakeCSV(employees);
-        Util.MakeBadges(employees);
+        Next(employees);
       }
       if (response == false)
       {
         List<Employee> employees = PeopleFetcher.GetEmployees();
-        Util.PrintEmployees(employees);
-        Util.MakeCSV(employees);
-        Util.MakeBadges(employees);
+        Next(employees);
       }
     }
   }
